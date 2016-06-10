eb2adce15c2732366388ce13ce919bbe02d04362
shooga <avishag212@gmail.com>
2016-06-11 02:24:59 +0300

    countinue minheap

--- src/MinHeapTree.java
+++ src/MinHeapTree.java
@@ -15,18 +15,33 @@ public class MinHeapTree {
 	}
 	
 	private void Heapify(int index){
+		int l=UtilsClass.Left(index),r=UtilsClass.Right(index);
+		int min=index;
 		
+		if (l<Size && (arr[l].getY() < arr[min].getY()))
+			min=l;
+		if( r<=Size && (arr[r].getY() < arr[min].getY()))
+			min=r;
+		if (min!= index)
+			UtilsClass.swap(arr, index, min);
+			Heapify(min);
 	}
 	
 	public Point ExtractMin(){
-	
-		return null;
+		if (Size==0)
+			throw new RuntimeException("Empty Heap");
+		Point min=new Point(arr[1]);
+		arr[1]=new Point(arr[Size]);
+		Size--;
+		Heapify(1);
+		return min;
 	}
 	
 	public void HeapInsert(Point key){
-		
+		Size++;
+		arr[Size]= new Point(Integer.MIN_VALUE,Integer.MIN_VALUE);
+		IncreaseKey(Size, key);
 	}
-	
 	public Point HeapMin(){
 		return arr[1]; 
 	}
